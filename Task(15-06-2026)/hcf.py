c3d94a8c44118e727e8b384dba935458851708d8
Abhireddy1208 <abhireddythatiparthi@gmail.com>
2026-06-26 10:36:54 +0530

    task13-06-26

--- Task(15-06-2026)/hcf.py
+++
@@ -1,5 +0,0 @@
-def gcd(a, b):
-    small = a if a < b else b
-    for i in range(small, 0, -1):
-        if a % i == 0 and b % i == 0:
-            return i
--- binary-decimal.py
+++
@@ -1,7 +0,0 @@
-binary = [[1, 0, 1, 1]]
-decimal = 0
-power = len(binary[0]) - 1
-for i in binary[0]:
-    decimal = decimal + i * (2 ** power)
-    power -= 1
-print("Decimal:", decimal)
