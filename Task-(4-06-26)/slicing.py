ce8fd0ff52e2646f67a43ca4bfd161624ef4c6d9
Abhireddy1208 <abhireddythatiparthi@gmail.com>
2026-06-26 12:12:55 +0530

    slicing

---
+++ Task-(4-06-26)/slicing.py
@@ -0,0 +1,39 @@
+#1
+text = "PythonProgramming"
+print(text[:6])
+
+#2
+text = "PythonProgramming"
+print(text[6:])
+
+#3
+text = "PythonProgramming"
+print(text[2:8])
+
+#4
+text = "PythonProgramming"
+print(text[::2])
+
+#5
+text = "PythonProgramming"
+print(text[::3])
+
+#6
+text = "PythonProgramming"
+print(text[::-1])
+
+#7
+text = "PythonProgramming"
+print(text[::-2])
+
+#8
+text = "PythonProgramming"
+print(text[-5:])
+
+#9
+text = "PythonProgramming"
+print(text[1:-1])
+
+#10
+text = "PythonProgramming"
+print(text[4:14])
